7dc81865967b039ca2d7ce021a8f6cdf693d5ef7
NovaBear <60272013+NovaBear@users.noreply.github.com>
2021-12-15 05:55:29 -0800

    werking
    Voer eerst het GUI script uit en daarna het test script om te kijken of het gelukt is.

---
+++ gui voor script.ps1
@@ -0,0 +1,28 @@
+﻿#input (word alemaal in variabele gestoken)
+
+    #vraagt om de Computer host naam in te geven
+    [System.Reflection.Assembly]::LoadWithPartialName('Microsoft.VisualBasic') | Out-Null
+    $computer = [Microsoft.VisualBasic.Interaction]::InputBox("Geef de serverComputerName op.", "Computer")
+
+    #vraagt om de Dns zone naam in te geven
+    [System.Reflection.Assembly]::LoadWithPartialName('Microsoft.VisualBasic') | Out-Null
+    $Dns = [Microsoft.VisualBasic.Interaction]::InputBox("Geef de Zonename op.", "Dns")
+
+    #vraagt om de CSV file in te geven
+    [Microsoft.VisualBasic.Interaction]::MsgBox("geef het CSV bestand op")
+
+    $OpenFileDialog = New-Object System.Windows.Forms.OpenFileDialog -Property @{
+    initialDirectory = [environment]::GetFolderPath("Desktop")
+    filter = "CSV (*.csv) | *.csv"
+    }
+    $OpenFileDialog.ShowDialog() 
+    $csvfile =  $OpenFileDialog.FileName
+    
+      Import-Csv -path "$csvfile" | foreach{Add-DnsServerResourceRecordCName  -zonename $Dns -computername $computer -cname $_.cname -RRtype A}
+      $ailias = "$row{0}"
+      $naam = "$row{1}"
+      select-Object "$ailias,$naam"
+        Foreach ($row in $csv) {
+        if (item in $naam -eq "" ) {print = "er is een of meerdere blanko in de naam collom"
+          }
+        }
---
+++ test csv import.ps1
@@ -0,0 +1,13 @@
+﻿Function Get-FileName($InitialDirectory)
+    {
+        [System.Reflection.Assembly]::LoadWithPartialName("System.windows.forms") | Out-Null
+
+      $OpenFileDialog = New-Object System.Windows.Forms.OpenFileDialog
+      $OpenFileDialog.initialDirectory = $initialDirectory
+      $OpenFileDialog.filter = "CSV (*.csv) | *.csv"
+      $OpenFileDialog.ShowDialog() | Out-Null
+    }
+
+    $csvfile = Get-FileName $OpenFileDialog.FileName
+
+Import-Csv $csvfile -Delimiter "," | select $row{0}  
